8d2ddd8f95c1e8f41a6563e94eb0cd6e51491330
Joakim Fors <joakim.fors@trivector.se>
2020-06-26 20:13:57 +0200

    Use automatic server number

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if ! which -- "${1}"; then
   # first arg is not an executable
-  xvfb-run --server-args="-screen 0 1024x768x24" -- node /app/ "$@"
+  xvfb-run -a --server-args="-screen 0 1024x768x24" -- node /app/ "$@"
   exit $?
 fi
 
