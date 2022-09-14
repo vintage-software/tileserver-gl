773902bbe908fbe1ed9e4bccb07eca610e8bcb2d
acalcutt <acalcutt@techidiots.net>
2022-09-14 17:43:44 -0400

    update docker images to work with new maplibre-native

--- publish.js
+++ publish.js
@@ -22,18 +22,19 @@ var packageJson = require('./package');
 packageJson.name += '-light';
 packageJson.description = 'Map tile server for JSON GL styles - serving vector tiles';
 delete packageJson.dependencies['canvas'];
-delete packageJson.dependencies['@mapbox/mapbox-gl-native'];
+delete packageJson.dependencies['@maplibre/maplibre-gl-native'];
 delete packageJson.dependencies['sharp'];
 
 delete packageJson.optionalDependencies;
 delete packageJson.devDependencies;
 
-packageJson.engines.node = '>= 10';
+packageJson.engines.node = '>= 14.13.0';
 
 var str = JSON.stringify(packageJson, undefined, 2);
 fs.writeFileSync('light/package.json', str);
 fs.renameSync('light/README_light.md', 'light/README.md');
 fs.renameSync('light/Dockerfile_light', 'light/Dockerfile');
+fs.renameSync('light/docker-entrypoint_light.sh', 'light/docker-entrypoint.sh');
 
 // for Build tileserver-gl-light docker image, don't publish
 if (process.argv.length > 2 && process.argv[2] == "--no-publish") {
