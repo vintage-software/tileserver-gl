b4cd42b1865373bb0713daa35fb2030f81099dba
acalcutt <acalcutt@techidiots.net>
2022-09-14 21:36:03 -0400

    fix spacing

--- src/main.js
+++ src/main.js
@@ -19,56 +19,56 @@ if (args.length >= 3 && args[2][0] !== '-') {
 
 const { program } = require('commander');
 program
-    .description('tileserver-gl startup options')
-    .usage('tileserver-gl [mbtiles] [options]')
-    .option(
-        '--mbtiles <file>',
-        'MBTiles file (uses demo configuration);\n' +
-    '\t                  ignored if the configuration file is also specified',
-    )
-    .option(
-        '-c, --config <file>',
-        'Configuration file [config.json]',
-        'config.json',
-    )
-    .option(
-        '-b, --bind <address>',
-        'Bind address',
-    )
-    .option(
-        '-p, --port <port>',
-        'Port [8080]',
-        8080,
-        parseInt,
-    )
-    .option(
-        '-C|--no-cors',
-        'Disable Cross-origin resource sharing headers',
-    )
-    .option(
-        '-u|--public_url <url>',
-        'Enable exposing the server on subpaths, not necessarily the root of the domain',
-    )
-    .option(
-        '-V, --verbose',
-        'More verbose output',
-    )
-    .option(
-        '-s, --silent',
-        'Less verbose output',
-    )
-    .option(
-        '-l|--log_file <file>',
-        'output log file (defaults to standard out)',
-    )
-    .option(
-        '-f|--log_format <format>',
-        'define the log format:  https://github.com/expressjs/morgan#morganformat-options',
-    )
-    .version(
-        packageJson.version,
-        '-v, --version',
-    );
+  .description('tileserver-gl startup options')
+  .usage('tileserver-gl [mbtiles] [options]')
+  .option(
+    '--mbtiles <file>',
+    'MBTiles file (uses demo configuration);\n' +
+  '\t          ignored if the configuration file is also specified',
+  )
+  .option(
+    '-c, --config <file>',
+    'Configuration file [config.json]',
+    'config.json',
+  )
+  .option(
+    '-b, --bind <address>',
+    'Bind address',
+  )
+  .option(
+    '-p, --port <port>',
+    'Port [8080]',
+    8080,
+    parseInt,
+  )
+  .option(
+    '-C|--no-cors',
+    'Disable Cross-origin resource sharing headers',
+  )
+  .option(
+    '-u|--public_url <url>',
+    'Enable exposing the server on subpaths, not necessarily the root of the domain',
+  )
+  .option(
+    '-V, --verbose',
+    'More verbose output',
+  )
+  .option(
+    '-s, --silent',
+    'Less verbose output',
+  )
+  .option(
+    '-l|--log_file <file>',
+    'output log file (defaults to standard out)',
+  )
+  .option(
+    '-f|--log_format <format>',
+    'define the log format:  https://github.com/expressjs/morgan#morganformat-options',
+  )
+  .version(
+    packageJson.version,
+    '-v, --version',
+  );
 program.parse(process.argv);
 const opts = program.opts();
 
