efb4b2c915b0c2d2b51d64bc2d87b4213ecb9091
acalcutt <acalcutt@techidiots.net>
2022-09-14 21:39:35 -0400

    orig formatting

--- src/main.js
+++ src/main.js
@@ -24,51 +24,51 @@ program
   .option(
     '--mbtiles <file>',
     'MBTiles file (uses demo configuration);\n' +
-  '\t          ignored if the configuration file is also specified',
+    '\t                  ignored if the configuration file is also specified'
   )
   .option(
     '-c, --config <file>',
     'Configuration file [config.json]',
-    'config.json',
+    'config.json'
   )
   .option(
     '-b, --bind <address>',
-    'Bind address',
+    'Bind address'
   )
   .option(
     '-p, --port <port>',
     'Port [8080]',
     8080,
-    parseInt,
+    parseInt
   )
   .option(
     '-C|--no-cors',
-    'Disable Cross-origin resource sharing headers',
+    'Disable Cross-origin resource sharing headers'
   )
   .option(
     '-u|--public_url <url>',
-    'Enable exposing the server on subpaths, not necessarily the root of the domain',
+    'Enable exposing the server on subpaths, not necessarily the root of the domain'
   )
   .option(
     '-V, --verbose',
-    'More verbose output',
+    'More verbose output'
   )
   .option(
     '-s, --silent',
-    'Less verbose output',
+    'Less verbose output'
   )
   .option(
     '-l|--log_file <file>',
-    'output log file (defaults to standard out)',
+    'output log file (defaults to standard out)'
   )
   .option(
     '-f|--log_format <format>',
-    'define the log format:  https://github.com/expressjs/morgan#morganformat-options',
+    'define the log format:  https://github.com/expressjs/morgan#morganformat-options'
   )
   .version(
     packageJson.version,
-    '-v, --version',
-  );
+    '-v, --version'
+  )
 program.parse(process.argv);
 const opts = program.opts();
 
